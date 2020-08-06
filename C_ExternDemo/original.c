9262ceb207768e367e28e394328c427021160554
connorLuca <connormccrum7@gmail.com>
2020-08-06 15:31:28 -0600

    demo done ver

--- C_ExternDemo/original.c
+++ C_ExternDemo/original.c
@@ -3,4 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+#include <stdio.h>
+int i=48;
+//declare variable here no body for code necessary as nothing is happening here
+//some large projects may have files just like this to store many variables to be used in the project.
